d8b1d71ed7c2b53c3a32ca63ac94fce8995b2b3a
DIGVIJAY <singhdigvijay703@gmail.com>
2021-06-18 12:05:32 +0530

    ..

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const api_config = {
-    port: 3000,
+    port: process.env.PORT,
     conn_url: 'mongodb+srv://valiantguardian:golu123@valiantguardiankaaasmaa.xkihd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     stripe_sk: 'sk_test_51Ix4d6SFVvoENMxpwdsmBrJmle3P4B971C7VpYx1Yl8qUR2d1KWIvxb6VY2ggi5ceYZtQJasM4wSwT2RZFVF5Sjw00xUxC9MYE',
     email: 'singhdigvijay703@gmail.com',
